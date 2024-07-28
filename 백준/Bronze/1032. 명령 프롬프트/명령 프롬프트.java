b2a863f8af0aa283ba0f686dd8bde181257d7e20
haeseung-lee <67096881+haeseung-lee@users.noreply.github.com>
2024-07-28 19:15:30 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -1,28 +1,39 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
         int n = Integer.parseInt(br.readLine());
-        String[] arr = new String[n];  //파일 이름 저장하는 배열
-        for (int i = 0; i < n; i++) {
-            arr[i] = br.readLine();  //파일 이름 배열에 저장하기
-        }
-        int len = arr[0].length(); 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            boolean b = true;  //모든 파일의 i번째 단어가 같으면 true, 다르면 false
-            char c = arr[0].charAt(i);  //첫 파일의 i번째 단어
-            for (int j = 1; j < n; j++) {
-                if (c != arr[j].charAt(i)) b = false;  //파일의 단어가 다르면 false로 바꿔주기
+        if(n == 1){
+            sb.append(br.readLine());
+        } else{
+            String[] arr = new String[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = br.readLine();
             }
-            if (b) {
-                sb.append(c);  //모든 파일의 단어가 같으면 그 단어 출력
-            } else {
-                sb.append("?");  //다르면 ? 출력
+            for(int i = 0; i < arr[0].length(); i++){
+                boolean a = true;
+                char txt = arr[0].charAt(i);
+                for(int j = 1; j < n; j++){
+                    if(txt != arr[j].charAt(i)){
+                        a = false;
+                    }
+                }
+                if(a){
+                    sb.append(txt);
+                } else{
+                    sb.append("?");
+                }
             }
         }
-        System.out.print(sb);
+        System.out.println(sb.toString());
+    
     }
 }
