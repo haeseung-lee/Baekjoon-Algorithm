e6a17ea3335df0c33c396eaa9f753899a718246e
haeseung-lee <67096881+haeseung-lee@users.noreply.github.com>
2024-07-31 00:00:54 +0900

    [Bronze I] Title: 약수, Time: 132 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+        for(int i = 1; i < n; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println(max * min);
+    }
+}
